3a54f9415be1ec4d598408240827cf81e604d30d
natatheleader <natymahetem@gmail.com>
2021-12-31 05:49:29 +0300

    cards are functional except description and image

--- src/components/card.vue
+++ src/components/card.vue
@@ -27,14 +27,5 @@ export default {
   created () {
     console.log(this.movie)
   }
-  // filters: {
-  //   truncate: function (text, length, suffix) {
-  //     if (text.length > length) {
-  //       return text.substring(0, length) + suffix
-  //     } else {
-  //       return text
-  //     }
-  //   }
-  // }
 }
 </script>
