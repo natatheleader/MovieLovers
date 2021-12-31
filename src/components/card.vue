4416fc74de583f49c7b9ab316ef0f47eced9c865
natatheleader <natymahetem@gmail.com>
2021-12-31 05:20:01 +0300

    install axios and update card with fetched data

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="m-5 flex-auto">
-    <div class="h-64 min-w-min bg-secondary">
-        <div class="bg-black bg-opacity-50">
-            <div class="w-32">
-                <div>3</div>
-            </div>
+  <div class="m-5 text-white">
+    <div class="h-64 min-w-min bg-secondary relative rounded-lg">
+      <img class="h-full w-full rounded-lg" src="https://m.media-amazon.com/images/M/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg" />
+      <div class="bg-black bg-opacity-50 absolute inset-x-0 bottom-0 rounded-b-lg">
+        <div class="m-1">
+          <p class="text-base font-bold">{{ t_title }}</p>
+          <p class="">{{ description }}</p>
+          <ul class="flex justify-center">
+            <li><i class="fas fa-star fa-sm text-yellow-500 mr-1"></i></li>
+            <li><i class="fas fa-star fa-sm text-yellow-500 mr-1"></i></li>
+            <li><i class="fas fa-star fa-sm text-yellow-500 mr-1"></i></li>
+            <li><i class="far fa-star fa-sm text-yellow-500 mr-1"></i></li>
+            <li><i class="far fa-star fa-sm text-yellow-500 mr-1"></i></li>
+            <p> {{ rating }}</p>
+          </ul>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Card'
+  props: ['m_title', 'description', 'rating', 'image'],
+  name: 'Card',
+  created () {
+    console.log(this.movie)
+  }
+  // filters: {
+  //   truncate: function (text, length, suffix) {
+  //     if (text.length > length) {
+  //       return text.substring(0, length) + suffix
+  //     } else {
+  //       return text
+  //     }
+  //   }
+  // }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="bg-primary h-full">
     <h3 class="text-white p-8 font-bold">Trending Movies</h3>
-    <div class="flex flex-row flex-wrap">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div class="grid grid-cols-3 gap-3">
+      <Card v-for="movie in movies" :key="movie.id" :image="movie.background_image" :rating="movie.rating" :description="movie.description_full" :m_title="movie.title_long" />
     </div>
     <h3 class="text-white p-8 font-bold">Trending Shows</h3>
-    <Card />
+    <div class="grid grid-cols-3 gap-3">
+      <Card />
+    </div>
   </div>
 </template>
 
 <script>
 import Card from '@/components/card.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
   components: {
     Card
+  },
+  data () {
+    return {
+      movies: [],
+      status: false,
+      status_message: '',
+      total_movies: 0
+    }
+  },
+  created () {
+    axios.get('https://yts.mx/api/v2/list_movies.json?limit=9').then(response => {
+      this.movies = response.data.data.movies
+      this.status = response.data.status
+      this.status_message = response.data.status_message
+      this.total_movies = response.data.data.movie_count
+    }).catch(error => {
+      console.log('There is an Error : ' + error.response)
+    })
   }
 }
 </script>
